4d5d33117481c172f13dcd08411d5080638949b2
MikuroXina <ryosukadnak@gmail.com>
2021-12-11 18:10:21 +0900

    Improve zipper current into Option

--- src/zipper.rs
+++ src/zipper.rs
@@ -1,15 +1,16 @@
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct Zipper<T> {
     left: Vec<T>,
-    current: T,
+    current: Option<T>,
     right: Vec<T>,
 }
 
-impl<T: Default> FromIterator<T> for Zipper<T> {
+impl<T> FromIterator<T> for Zipper<T> {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut iter = iter.into_iter();
         Self {
             left: vec![],
-            current: T::default(),
+            current: iter.next(),
             right: Vec::from_iter(iter),
         }
     }
@@ -17,46 +18,61 @@ impl<T: Default> FromIterator<T> for Zipper<T> {
 
 impl<T> Zipper<T> {
     pub fn map<U>(self, f: impl Fn(T) -> U) -> Zipper<U> {
-        let current = f(self.current);
         Zipper {
             left: self.left.into_iter().map(&f).collect(),
-            current,
+            current: self.current.map(&f),
             right: self.right.into_iter().map(f).collect(),
         }
     }
 
-    pub fn top(&self) -> (Option<&T>, &T, Option<&T>) {
-        (self.left.last(), &self.current, self.right.last())
+    pub fn top(&self) -> (Option<&T>, Option<&T>, Option<&T>) {
+        (self.left.last(), self.current.as_ref(), self.right.last())
+    }
+
+    pub fn iter(&self) -> ZipperIter<T> {
+        ZipperIter {
+            left: self.left.iter(),
+            current: self.current.as_ref(),
+            right: self.right.iter(),
+        }
     }
 }
 
 impl<T: Clone> Zipper<T> {
     pub fn new(size: usize, init: T) -> Self {
-        Self {
-            left: vec![init.clone(); size],
-            current: init.clone(),
-            right: vec![init; size],
+        if size == 0 {
+            Self {
+                left: vec![],
+                current: None,
+                right: vec![],
+            }
+        } else {
+            Self {
+                left: vec![],
+                current: Some(init.clone()),
+                right: vec![init; size - 1],
+            }
         }
     }
 
     pub fn left(&self) -> Option<Self> {
         let mut cloned = self.clone();
-        let current = cloned.left.pop()?;
-        let right = std::mem::replace(&mut cloned.current, current);
+        let current = cloned.left.pop();
+        let right = std::mem::replace(&mut cloned.current, current)?;
         cloned.right.push(right);
         Some(cloned)
     }
 
     pub fn right(&self) -> Option<Self> {
         let mut cloned = self.clone();
-        let current = cloned.right.pop()?;
-        let left = std::mem::replace(&mut cloned.current, current);
+        let current = cloned.right.pop();
+        let left = std::mem::replace(&mut cloned.current, current)?;
         cloned.left.push(left);
         Some(cloned)
     }
 
     pub fn extract(&self) -> T {
-        self.current.clone()
+        self.current.as_ref().cloned().unwrap()
     }
 
     pub fn duplicate(&self) -> Zipper<Self> {
@@ -68,12 +84,30 @@ impl<T: Clone> Zipper<T> {
             .collect();
         Zipper {
             left,
-            current: self.clone(),
+            current: Some(self.clone()),
             right,
         }
     }
 
     pub fn extend<U>(&self, f: impl Fn(Self) -> U) -> Zipper<U> {
         self.duplicate().map(f)
+    }
+}
+
+#[derive(Debug)]
+pub struct ZipperIter<'z, T> {
+    left: std::slice::Iter<'z, T>,
+    current: Option<&'z T>,
+    right: std::slice::Iter<'z, T>,
+}
+
+impl<'z, T> Iterator for ZipperIter<'z, T> {
+    type Item = &'z T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.left
+            .next()
+            .or_else(|| self.current.take())
+            .or_else(|| self.right.next_back())
     }
 }
